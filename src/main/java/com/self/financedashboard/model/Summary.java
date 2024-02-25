4b39c065cf1bdd76e17f724aee82febac0793091
Prashanth Kumar <prashusiri.1990@gmail.com>
2024-02-25 20:35:00 +0530

    Using jakarta

--- src/main/java/com/self/financedashboard/model/Summary.java
+++ src/main/java/com/self/financedashboard/model/Summary.java
@@ -1,15 +1,13 @@
 package com.self.financedashboard.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
--- src/main/java/com/self/financedashboard/model/UserLogin.java
+++ src/main/java/com/self/financedashboard/model/UserLogin.java
@@ -1,17 +1,11 @@
 package com.self.financedashboard.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
 
 @AllArgsConstructor
 @NoArgsConstructor
